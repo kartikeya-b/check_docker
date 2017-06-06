e7c825c7cb6f33cc68a506242c69961f14fd98f3
kartikeya-b <bahuguna.kartikeya@gmail.com>
2017-06-06 15:11:18 +0530

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # MAINTAINER Ponnusamy<pananthasankaranarayanan@sapient.com>
 # cat Dockerfile | sed "s/{{app_version}}/$1/" | 
-docker build -t tomcat-deploy .
+sudo docker build -t tomcat-deploy .
 sleep 20
-docker run --name tom -p 0.0.0.0:9080:80 -d tomcat-deploy
+sudo docker run --name tom -p 0.0.0.0:9080:80 -d tomcat-deploy
